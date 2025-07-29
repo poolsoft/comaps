c014e224b2b0ed2e8b7e1532e9ceb5a5c85b8ed5
Andrei Shkrob <github@shkrob.dev>
2025-07-29 22:54:24 +0200

    [routing] New API for lanes
    Signed-off-by: Andrei Shkrob <github@shkrob.dev>

--- android/app/src/main/java/app/organicmaps/car/util/RoutingHelpers.java
+++ android/app/src/main/java/app/organicmaps/car/util/RoutingHelpers.java
@@ -31,18 +31,19 @@ public final class RoutingHelpers
   @NonNull
   public static LaneDirection createLaneDirection(@NonNull LaneWay laneWay, boolean isRecommended)
   {
-    int shape = LaneDirection.SHAPE_UNKNOWN;
-    shape = switch (laneWay)
+    @LaneDirection.Shape
+    final int shape = switch (laneWay)
     {
-      case REVERSE -> LaneDirection.SHAPE_U_TURN_LEFT;
-      case SHARP_LEFT -> LaneDirection.SHAPE_SHARP_LEFT;
-      case LEFT -> LaneDirection.SHAPE_NORMAL_LEFT;
-      case SLIGHT_LEFT, MERGE_TO_LEFT -> LaneDirection.SHAPE_SLIGHT_LEFT;
-      case SLIGHT_RIGHT, MERGE_TO_RIGHT -> LaneDirection.SHAPE_SLIGHT_RIGHT;
-      case THROUGH -> LaneDirection.SHAPE_STRAIGHT;
-      case RIGHT -> LaneDirection.SHAPE_NORMAL_RIGHT;
-      case SHARP_RIGHT -> LaneDirection.SHAPE_SHARP_RIGHT;
-      default -> shape;
+      case ReverseLeft -> LaneDirection.SHAPE_U_TURN_LEFT;
+      case SharpLeft -> LaneDirection.SHAPE_SHARP_LEFT;
+      case Left -> LaneDirection.SHAPE_NORMAL_LEFT;
+      case MergeToLeft, SlightLeft -> LaneDirection.SHAPE_SLIGHT_LEFT;
+      case Through -> LaneDirection.SHAPE_STRAIGHT;
+      case SlightRight, MergeToRight -> LaneDirection.SHAPE_SLIGHT_RIGHT;
+      case Right -> LaneDirection.SHAPE_NORMAL_RIGHT;
+      case SharpRight -> LaneDirection.SHAPE_SHARP_RIGHT;
+      case ReverseRight -> LaneDirection.SHAPE_U_TURN_RIGHT;
+      default -> LaneDirection.SHAPE_UNKNOWN;
     };
 
         return LaneDirection.create(shape, isRecommended);
