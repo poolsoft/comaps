3aa74bc4274c6e3131084bdd86303c3aa9cf3ba6
Alexander Borsuk <me@alex.bio>
2025-07-31 21:50:27 +0200

    [ios] Fixed ATM translation in the PlacePage
    Signed-off-by: Alexander Borsuk <me@alex.bio>

--- iphone/CoreApi/CoreApi/PlacePageData/Common/PlacePageInfoData.mm
+++ iphone/CoreApi/CoreApi/PlacePageData/Common/PlacePageInfoData.mm
@@ -100,7 +100,7 @@ NSString * GetLocalizedMetadataValueString(MapObject::MetadataID metaID, std::st
       }
     });
 
-    _atm = rawData.HasAtm() ? NSLocalizedString(@"type.amenity.atm", nil) : nil;
+    _atm = rawData.HasAtm() ? NSLocalizedStringFromTable(@"type.amenity.atm", @"LocalizableTypes", nil) : nil;
 
     _address = rawData.GetSecondarySubtitle().empty() ? nil : @(rawData.GetSecondarySubtitle().c_str());
     _coordFormats = @[@(rawData.GetFormattedCoordinate(place_page::CoordinatesFormat::LatLonDMS).c_str()),
