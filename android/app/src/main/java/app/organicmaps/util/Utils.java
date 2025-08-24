organicmaps/organicmaps

[android][sdk] Move getDonateUrl outside sdk
Signed-off-by: Andrei Shkrob <github@shkrob.dev>

android/app/src/main/java/app/organicmaps/util/Utils.java
@@ -39,6 +39,7 @@ import app.organicmaps.BuildConfig;
 import app.organicmaps.MwmActivity;
 import app.organicmaps.MwmApplication;
 import app.organicmaps.R;
+import app.organicmaps.sdk.util.Config;
 import app.organicmaps.sdk.util.Constants;
 import app.organicmaps.sdk.util.Distance;
 import app.organicmaps.sdk.util.StringUtils;
@@ -494,5 +495,16 @@ public class Utils
   {
     final LocalTime time = LocalTime.now().plusSeconds(seconds);
     return StringUtils.formatUsingUsLocale("%d:%02d", time.getHour(), time.getMinute());
+  }
+
+  @NonNull
+  public static String getDonateUrl(@NonNull Context context)
+  {
+    final String url = Config.getDonateUrl();
+    // Enable donations by default if not Google or Huawei. Replace organicmaps.app/donate/ with localized page.
+    if ((url.isEmpty() && !BuildConfig.FLAVOR.equals("google") && !BuildConfig.FLAVOR.equals("huawei"))
+        || url.endsWith("organicmaps.app/donate/"))
+      return context.getString(R.string.translated_om_site_url) + "donate/";
+    return url;
   }
 }

poolsoft/comaps

android/app/src/main/java/app/organicmaps/util/Utils.java
@@ -39,6 +39,7 @@ import app.organicmaps.BuildConfig;
 import app.organicmaps.MwmActivity;
 import app.organicmaps.MwmApplication;
 import app.organicmaps.R;
+import app.organicmaps.sdk.util.Config;
 import app.organicmaps.sdk.util.Constants;
 import app.organicmaps.sdk.util.Distance;
 import app.organicmaps.sdk.util.StringUtils;
@@ -481,5 +482,16 @@ public class Utils
   {
     final LocalTime time = LocalTime.now().plusSeconds(seconds);
     return StringUtils.formatUsingUsLocale("%d:%02d", time.getHour(), time.getMinute());
+  }
+
+  @NonNull
+  public static String getDonateUrl(@NonNull Context context)
+  {
+    final String url = Config.getDonateUrl();
+    // Enable donations by default if not Google or Huawei. Replace comaps.app/donate/ with localized page.
+    if ((url.isEmpty() && !BuildConfig.FLAVOR.equals("google") && !BuildConfig.FLAVOR.equals("huawei"))
+        || url.endsWith("comaps.app/donate/"))
+      return context.getString(R.string.app_site_url) + "donate/";
+    return url;
   }
 }