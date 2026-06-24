a8ec9a69afa782e92d03155e0f7f7b97ec673d7e
mgulacti <muhammedgulacti@kahramanmaras.bel.tr>
2026-06-24 09:38:54 +0300

    Refactor: Delete all faulty Car Launcher modules and set up basic frame

--- android/app/src/carlauncher/java/app/organicmaps/carlauncher/CarLauncherActivity.java
+++ android/app/src/carlauncher/java/app/organicmaps/carlauncher/CarLauncherActivity.java
@@ -4,81 +4,35 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import app.organicmaps.MwmActivity;
 import app.organicmaps.R;
-import app.organicmaps.carlauncher.ui.PanelContentManager;
 
 public class CarLauncherActivity extends MwmActivity implements CarLauncherInterface {
     
-    private PanelContentManager panelContentManager;
-
     @Override
     protected void onSafeCreate(@Nullable Bundle savedInstanceState) {
-        // MwmActivity onSafeCreate setContentView(R.layout.activity_map) cagirir.
         super.onSafeCreate(savedInstanceState);
         
-        // MwmActivity layout'u uzerinde CarLauncher icin gerekli panel container'ini
-        // olusturabilir veya varolan bir container id'sini kullanabiliriz.
-        // Ornegin, MwmActivity'nin layout'unda R.id.fragment_container_view vs kullanabiliriz.
-        // Sadece ornekleme amaciyla PanelContentManager'i baslatiyoruz:
-        // panelContentManager = new PanelContentManager(getSupportFragmentManager(), R.id.map_fragment_container);
-        // NOT: Gercek containerId projeye gore ayarlanmalidir.
+        // Base layout'u ekle
+        setContentView(R.layout.activity_car_launcher);
     }
 
     @Override
-    public void openAppDrawer() {
-        if (panelContentManager != null) {
-            panelContentManager.setContent(PanelContentManager.PanelContent.APP_DRAWER);
-        }
-    }
+    public void openAppDrawer() {}
 
     @Override
-    public void closeAppDrawer() {
-        if (panelContentManager != null) {
-            panelContentManager.setContent(PanelContentManager.PanelContent.WIDGETS);
-        }
-    }
+    public void closeAppDrawer() {}
 
     @Override
-    public void openMusicPlayer() {
-        if (panelContentManager != null) {
-            panelContentManager.setContent(PanelContentManager.PanelContent.MUSIC);
-        }
-    }
+    public void openMusicPlayer() {}
 
     @Override
-    public void openWeatherDashboard() {
-        if (panelContentManager != null) {
-            panelContentManager.setContent(PanelContentManager.PanelContent.WEATHER);
-        }
-    }
+    public void openWeatherDashboard() {}
 
     @Override
-    public void setPanelContent(PanelContentManager.PanelContent content) {
-        if (panelContentManager != null) {
-            panelContentManager.setContent(content);
-        }
-    }
+    public void onLayoutModeToggle() {}
 
     @Override
-    public void onLayoutModeToggle() {
-        // Layout mode toggle implementasyonu
-    }
+    public void onDesktopModeToggle() {}
 
     @Override
-    public void onDesktopModeToggle() {
-        if (panelContentManager != null) {
-            panelContentManager.setContent(PanelContentManager.PanelContent.DESKTOP);
-        }
-    }
-
-    @Override
-    public void openCarLauncherSettings() {
-        if (panelContentManager != null) {
-            panelContentManager.setContent(PanelContentManager.PanelContent.SETTINGS);
-        }
-    }
-
-    @Override
-    public void checkAndRefreshDockFragmentIfNeeded() {
-        // Dock fragment guncellemesi
-    }
+    public void openCarLauncherSettings() {}
 }
--- android/app/src/carlauncher/java/app/organicmaps/carlauncher/CarLauncherInterface.java
+++ android/app/src/carlauncher/java/app/organicmaps/carlauncher/CarLauncherInterface.java
@@ -1,30 +1,11 @@
 package app.organicmaps.carlauncher;
 
-import app.organicmaps.carlauncher.ui.PanelContentManager;
-
-
-/**
- * Interface to expose CarLauncher specific methods from MwmActivity.
- */
 public interface CarLauncherInterface {
     void openAppDrawer();
-
     void closeAppDrawer();
-
     void openMusicPlayer();
-    
     void openWeatherDashboard();
-
-    
-
-    /**
-     * SaÃ„Å¸ panel iÃƒÂ§eriÃ„Å¸ini deÃ„Å¸iÃ…Å¸tirir (Android Auto UI).
-     * @param content PanelContent enum deÃ„Å¸eri
-     */
-    void setPanelContent(PanelContentManager.PanelContent content);
-
     void onLayoutModeToggle();
     void onDesktopModeToggle();
     void openCarLauncherSettings();
-    void checkAndRefreshDockFragmentIfNeeded();
 }
