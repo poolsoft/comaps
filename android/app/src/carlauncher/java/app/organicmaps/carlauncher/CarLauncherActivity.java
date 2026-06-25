6899c4d7e476f51ffec619a97510e3c1689b270e
mgulacti <muhammedgulacti@kahramanmaras.bel.tr>
2026-06-25 11:37:37 +0300

    Initialize Car Layout Manager and hide original MwmActivity buttons

--- android/app/src/carlauncher/java/app/organicmaps/carlauncher/CarLauncherActivity.java
+++ android/app/src/carlauncher/java/app/organicmaps/carlauncher/CarLauncherActivity.java
@@ -13,9 +13,14 @@ import android.widget.ImageButton;
 import android.graphics.Bitmap;
 import app.organicmaps.carlauncher.music.MusicManager;
 
+import app.organicmaps.carlauncher.ui.CarLayoutManager;
+import app.organicmaps.carlauncher.ui.AppDockFragment;
+import app.organicmaps.carlauncher.ui.WidgetPanelFragment;
+
 public class CarLauncherActivity extends MwmActivity implements CarLauncherInterface, TelemetryManager.TelemetryListener {
     
     private TelemetryManager telemetryManager;
+    private CarLayoutManager layoutManager;
 
     @Override
     protected int getLayoutResId() {
@@ -28,6 +33,26 @@ public class CarLauncherActivity extends MwmActivity implements CarLauncherInter
         // setContentView(R.layout.activity_car_launcher); artik gerek yok cunku getLayoutResId uzerinden MwmActivity yukluyor.
 
         telemetryManager = TelemetryManager.getInstance(this);
+
+        layoutManager = new CarLayoutManager(this);
+        layoutManager.applyLayout(true, 1);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                .replace(R.id.app_dock, new AppDockFragment())
+                .replace(R.id.widget_panel, new WidgetPanelFragment())
+                .commitAllowingStateLoss();
+        }
+
+        // Orijinal MwmActivity arayuz bilesenlerini (arama, favoriler, menu vs) Car Launcher'da gizle (Turkce karakter yok)
+        android.view.View mapButtons = findViewById(R.id.map_buttons);
+        if (mapButtons != null) mapButtons.setVisibility(android.view.View.GONE);
+
+        android.view.View toolbar = findViewById(R.id.toolbar);
+        if (toolbar != null) toolbar.setVisibility(android.view.View.GONE);
+
+        android.view.View menu = findViewById(R.id.menu_frame); // Eger ID menu_frame ise (activity_map'te menu id'li include)
+        if (menu != null) menu.setVisibility(android.view.View.GONE);
     }
 
     @Override
